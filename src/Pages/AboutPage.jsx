1813d67d41f606ce426ff86839a11a3bf5660a1b
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-26 15:34:15 +0500

    first commit

---
+++ src/Pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer, Navbar } from '../components'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Footer />
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/Pages/ContactPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer, Navbar } from '../components'
+
+const ContactPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Footer />
+    </div>
+  )
+}
+
+export default ContactPage
